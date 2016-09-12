244dc5a78f2818ee6e6da792bc4401585adcce42
Adam Wang <hwang12wou@gmail.com>
2016-09-12 15:59:47 -0400

    concatenating css

--- staticserver/gulpfile.js
+++ staticserver/gulpfile.js
@@ -1,32 +1,39 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
+var concat = require('gulp-concat');
 //file path
+var DISCT_PATH = 'public/dist';
 var SCRIPT_PATH = 'public/scripts/**/*.js'
-
+var CSS_PATH = 'public/css/**/*.css';
 //Styles -> css
-gulp.task('styles', function(){
-  console.log('starting styles task');
+gulp.task('styles', function() {
+    console.log('starting styles task');
+    //return gulp.src(CSS_PATH)
+      return gulp.src(['public/css/reset.css',CSS_PATH])//use array to specify which css load first
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest(DISCT_PATH))
+      .pipe(livereload());
 });
 
 //Scripts -> js
-gulp.task('scripts', function(){
-  console.log('starting scripts task');
+gulp.task('scripts', function() {
+    console.log('starting scripts task');
 
-  return gulp.src(SCRIPT_PATH)
-    .pipe(uglify())
-    .pipe(gulp.dest('public/dist'))
-    .pipe(livereload());
+    return gulp.src(SCRIPT_PATH)
+        .pipe(uglify())
+        .pipe(gulp.dest(DISCT_PATH))
+        .pipe(livereload());
 });
 
 //images -> compress img
 
 //default -> bootstrap other tasks
-gulp.task('default', function(){
-  console.log('starting default task');
+gulp.task('default', function() {
+    console.log('starting default task');
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
     console.log('starting watch task');
     require('./server.js');
     livereload.listen();
